66cbab1c88a843945282a4284ed3c5c4b7527d04
darryl <darrylmangibin@gmail.com>
2022-12-01 16:43:48 +0800

    Add pagination and filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,16 +1,79 @@
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 
 // @description Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access PUBLIC
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-	const bootcamps = await Bootcamp.find();
+	let query;
 
-	res
-		.status(200)
-		.json({ success: true, count: bootcamps.length, data: bootcamps });
+	const reqQuery = { ...req.query };
+
+	const removeFields = ['select', 'sort', 'page', 'limit'];
+
+	removeFields.forEach((param) => {
+		delete reqQuery;
+	});
+
+	let queryStr = JSON.stringify(reqQuery);
+
+	queryStr = queryStr.replace(
+		/\b(gt|gte|lt|lte|in)\b/g,
+		(match) => `$${match}`,
+	);
+
+	query = Bootcamp.find(JSON.parse(queryStr));
+
+	if (req.query.select) {
+		const fields = req.query.select.split(',').join(' ');
+
+		query = query.select(fields);
+	}
+
+	if (req.query.sort) {
+		const sortBy = req.query.sort.split(',').join(' ');
+
+		query = query.sort(sortBy);
+	} else {
+		query = query.sort('-createdAt');
+	}
+
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 10;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	const total = await Bootcamp.countDocuments();
+
+	query = query.skip(startIndex).limit(limit);
+
+	const bootcamps = await query;
+
+	const pagination = {};
+
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit,
+		};
+	}
+
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit,
+		};
+	}
+
+	console.log(pagination);
+
+	res.status(200).json({
+		success: true,
+		count: bootcamps.length,
+		pagination,
+		data: bootcamps,
+	});
 });
 
 // @description Get single bootcamps
@@ -71,4 +134,29 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 	}
 
 	res.status(200).json({ success: true, data: bootcamp });
+});
+
+// @description Get bootcamps within a radius
+// @route GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access PRIVATE
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+	const { zipcode, distance } = req.params;
+
+	const loc = await geocoder.geocode(zipcode);
+	const lat = loc[0].latitude;
+	const lng = loc[0].longitude;
+
+	const radius = distance / 3963;
+
+	const bootcamps = await Bootcamp.find({
+		location: {
+			$geoWithin: { $centerSphere: [[lng, lat], radius] },
+		},
+	});
+
+	res.status(200).json({
+		success: true,
+		count: bootcamps.length,
+		data: bootcamps,
+	});
 });
