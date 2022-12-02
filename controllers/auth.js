161b24648b751194b2200015c633a4eac505f940
darryl <darrylmangibin@gmail.com>
2022-12-02 12:17:15 +0800

    User login

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,3 +22,33 @@ exports.register = asyncHandler(async (req, res, next) => {
 		token,
 	});
 });
+
+// @description Login User
+// @route POST /api/v1/auth/login
+// @access PUBLIC
+exports.login = asyncHandler(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		return next(new ErrorResponse('Please provide an email and password', 400));
+	}
+
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user) {
+		return next(new ErrorResponse('Invalid credentials', 401));
+	}
+
+	const isMatch = await user.matchPassword(password);
+
+	if (!isMatch) {
+		return next(new ErrorResponse('Invalid credentials', 401));
+	}
+
+	const token = user.getSignedJwtToken();
+
+	res.status(201).json({
+		success: true,
+		token,
+	});
+});
