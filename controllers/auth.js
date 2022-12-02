c19e1b715bc674eff8a0839454fa943d1b131d3e
darryl <darrylmangibin@gmail.com>
2022-12-02 11:59:30 +0800

    Encrypt password on register

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,16 @@ const asyncHandler = require('../middleware/async');
 // @route POST /api/v1/auth/register
 // @access PUBLIC
 exports.register = asyncHandler(async (req, res, next) => {
-	res.status(200).json({
+	const { name, email, password, role } = req.body;
+
+	const user = await User.create({
+		name,
+		email,
+		password,
+		role,
+	});
+
+	res.status(201).json({
 		success: true,
 	});
 });
