3d59ec79c8e90f5d0b16d22e4dcd0a9d9a52c9ce
darryl <darrylmangibin@gmail.com>
2022-12-03 03:42:57 +0800

    Add securities

--- controllers/auth.js
+++ controllers/auth.js
@@ -185,3 +185,18 @@ exports.updatePassword = asyncHandler(async (req, res, next) => {
 
 	sendTokenResponse(user, 200, res);
 });
+
+// @description logout
+// @route GET /api/v1/auth/logout
+// @access PRIVATE
+exports.logout = asyncHandler(async (req, res, next) => {
+	res.cookie('token', 'none', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+
+	res.status(200).json({
+		success: true,
+		data: {},
+	});
+});
--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 
@@ -26,9 +33,30 @@ const app = express();
 
 app.use(express.json());
 
+// Sanitize data
+app.use(mongoSanitize());
+// Security headers
+app.use(helmet());
+// Prevent xss attacks
+app.use(xss());
+// Rate limiting
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000,
+	max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http param polution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
 app.use(cookieParser());
 
 app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
