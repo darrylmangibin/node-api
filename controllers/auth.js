faa43e9dc8d4e1bf6718fa9239494ed4fdf13c3d
darryl <darrylmangibin@gmail.com>
2022-12-02 12:05:46 +0800

    Sign get jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,10 @@ exports.register = asyncHandler(async (req, res, next) => {
 		role,
 	});
 
+	const token = user.getSignedJwtToken();
+
 	res.status(201).json({
 		success: true,
+		token,
 	});
 });
